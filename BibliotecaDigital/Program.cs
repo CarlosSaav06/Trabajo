164ca16e8d11abab300f790769654ceb8d37854c
CarlosSaav06 <cfugarte@uamv.edu.ni>
2025-11-24 10:05:00 -0600

    Add project files.

---
+++ BibliotecaDigital/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BibliotecaDigital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
